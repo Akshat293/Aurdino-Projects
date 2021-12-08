4db81c425bae9e778ffb73efd31ec1bbce0edf32
Akshat Saxena <76419575+tom635@users.noreply.github.com>
2021-12-08 10:26:49 +0530

    Update Bluetooth_controlled_car.ino

--- Bluetooth_controlled_car/Bluetooth_controlled_car.ino
+++ Bluetooth_controlled_car/Bluetooth_controlled_car.ino
@@ -1,10 +1,17 @@
  #include <SoftwareSerial.h>
 SoftwareSerial bluetooth(2,3); // RX, TX
+#define echoPin 5 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 6 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define Buzpin 6
+
+
 char t;
  
 void setup() {
   Serial.begin(9600);
 bluetooth.begin(9600);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
 pinMode(10,OUTPUT);   //left motors forward
 pinMode(11,OUTPUT);   //left motors reverse
 pinMode(12,OUTPUT);   //right motors forward
@@ -16,6 +23,20 @@ if(bluetooth.available()>0){
   t = bluetooth.read();
   Serial.println(t);
 }
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+ if(distance<20){
+    digitalWrite(BuzPin, HIGH);
+      delay(1000);
+ }
  
 if(t == 'F'){            //move forward(all motors rotate in forward direction)
   digitalWrite(10,HIGH);
